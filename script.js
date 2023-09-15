dfed82e65fd27a576895723196384c226e85978a
Hannah <hannah9601@yahoo.com>
2023-09-15 04:21:24 -0400

    1

---
+++ script.js
@@ -0,0 +1,31 @@
+// function restyle(){
+//   var randomRed = Math.random() * 200;
+//   var randomGreen = Math.random() * 200;
+//   var randomBlue = Math.random() * 200;
+//
+//   var newColor = "radial-gradient(rgb(" + randomRed + "," + randomGreen + "," + randomBlue + "), rgb(" + randomRed + "," + randomGreen + "," + randomBlue + "), rgb(" + randomRed + "," + randomGreen + "," + randomBlue + "), rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ") ";
+//   outputShape.style.background = newColor;
+//
+// }
+
+
+// color change
+// var i = 0;
+// function change() {
+//   var circle = document.getElementById("gradientcircle");
+//   var color = ["radial-gradient(#ffad42,#ffb759, #fffbb5, #fffbb5)", "radial-gradient(orange, pink, #fffbb5, #fffbb5)", "radial-gradient(blue, pink, #fffbb5, #fffbb5)", " radial-gradient(orange, yellow, #fffbb5, #fffbb5)"];
+//   circle.style.background = color[i];
+//   i = (i + 1) % color.length;
+// }
+// setInterval(change, 3000);
+
+// size change
+// var i = 0;
+// function change() {
+//   var circle = document.getElementById("gradientcircle");
+//   var width = ["90vh","100vw", "110vw"];
+//   circle.style.width = width[i];
+//   circle.style.height = width[i];
+//   i = (i + 1) % width.length;
+// }
+// setInterval(change, 1000);
